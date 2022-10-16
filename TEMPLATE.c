17c83854f04667df99b19765bf2ccebea4e2898b
VmfadhilV <115922937+VmfadhilV@users.noreply.github.com>
2022-10-16 17:21:58 +0800

    Add files via upload

---
+++ TEMPLATE.c
@@ -0,0 +1,23 @@
+#include <stdio.h>      // do not delete
+#include <string.h>     // do not delete
+#include <unistd.h>     // do not delete
+#include <fcntl.h>      // do not delete
+#include <windows.h>    // do not delete
+#include "HW.h"         // do not delete
+// edit line 23 in HW.h for your com port. Default = COM3
+
+
+int main ()             // do not delete
+{
+    ComOn();            // do not delete
+
+    //Enter your programming below
+
+
+
+
+    //End your program above here
+
+    ComOff();           // do not delete
+    return 0;           // do not delete
+}
